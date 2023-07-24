f0d7c7004dc3a2a0439c10b2cbdbbd4b6639ca1f
Prathmesh Khade <91484952+Prathmeshkhade123@users.noreply.github.com>
2023-07-24 22:28:04 +0530

    Add files via upload

---
+++ springnoot-registration-login/src/test/java/com/example/demo/SpringnootRegistrationLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringnootRegistrationLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
